5b13b6571421cdf823797791e55c11cde058c103
m.a <comail205076@gmail.com>
2021-11-22 16:26:38 +0330

    state10

--- shohada/settings.py
+++ shohada/settings.py
@@ -14,7 +14,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-kv(n*r4lx1g!y09$r16dnzxa!h4q!p-q%cy3!84yce^_(x8%9n'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['whispering-cliffs-64993.herokuapp.com']
 
