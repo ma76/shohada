3c8fbdaa7f795c67e66a7b72d77104846b1f1f5c
m.a <comail205076@gmail.com>
2021-11-22 16:19:08 +0330

    state8

--- shohada/settings.py
+++ shohada/settings.py
@@ -16,7 +16,7 @@ SECRET_KEY = 'django-insecure-kv(n*r4lx1g!y09$r16dnzxa!h4q!p-q%cy3!84yce^_(x8%9n
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['whispering-cliffs-64993.herokuapp.com']
 
 
 # Application definition
